78e3b735d9166cdce6d290ad193721db3340b832
urvish <sojitraurvish0@gmail.com>
2024-01-09 17:14:15 +0530

    added real new redux tool kit finish

--- crwn-clothing-v2-redux-toolkit/src/store/store.js
+++ crwn-clothing-v2-redux-toolkit/src/store/store.js
@@ -1,6 +1,7 @@
-import { compose, createStore, applyMiddleware } from 'redux';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+// import { compose, createStore, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'; // this configureStore will take place of createStore and applyMiddleware
+// import { persistStore, persistReducer } from 'redux-persist';
+// import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
@@ -9,26 +10,47 @@ const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
 
-const composeEnhancer =
-  (process.env.NODE_ENV !== 'production' &&
-    window &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composeEnhancer =
+//   (process.env.NODE_ENV !== 'production' &&
+//     window &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['user'],
-};
+// const persistConfig = {
+//   key: 'root',
+//   storage,
+//   blacklist: ['user'],
+// };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+// const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(
-  persistedReducer,
-  undefined,
-  composedEnhancers
-);
+//(
 
-export const persistor = persistStore(store);
+// now redux tool kit includes three middleware by default
+// one of them is redux thunk so redux toolkit by default ships wit redux thunk
+export const store1 = configureStore({
+  reducer: rootReducer,
+  middleware:[logger] // if here you dont pass anything(our own middleware) then and only it use by default middleware and it is redux thunk
+})
+// now here the second by default middleware is siralizer that say that when you try to store value that are non siralize value(string number or js object)-> (mean js constractor then error) than i will through error 
+// here two ways we can ignore it or disabled it
+// if we pass arrry of middleware or own than it override all of the default middleware like [logger]
+// but we do want pass middleware and do not want do override of ridux than write like bellow
+
+// 3rd one has immutablility check mean with selecter fatch value you can not modify it
+//)
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware/*old all midderware thunk...,..*/) =>
+    getDefaultMiddleware(/*{}*/
+     /*{
+      serializableCheck:false,// now it will not check for serialize and if you want to ignore stroing constractor that firebase onject give us then go to file app.js pickedUser 
+     }*/
+    ).concat(middleWares),
+    //*{}* here we can pass object to overide default configrations
+  }); 
+
+// export const persistor = persistStore(store);
--- crwn-clothing-v2-redux-toolkit/src/store/user/user.reducer.js
+++ crwn-clothing-v2-redux-toolkit/src/store/user/user.reducer.js
@@ -1,16 +1,37 @@
-import { USER_ACTION_TYPES } from './user.types';
+import { createSlice } from '@reduxjs/toolkit';
 
 const INITIAL_STATE = {
   currentUser: null,
+  test: { a: 1 },
 };
 
-export const userReducer = (state = INITIAL_STATE, action) => {
-  const { type, payload } = action;
+export const userSlice = createSlice({
+  name: 'user',
+  initialState: INITIAL_STATE,
+  reducers: {
+    setCurrentUser(state, action) {
+      state.currentUser = action.payload; 
+      // in redux state is immutable but here it seems that we are mutating state but under the hood it create a brand new object this only mutation style and when it find mutation it create new object = assigning is mutation
+    },
+  },
+});
 
-  switch (type) {
-    case USER_ACTION_TYPES.SET_CURRENT_USER:
-      return { ...state, currentUser: payload };
-    default:
-      return state;
-  }
-};
+export const { setCurrentUser } = userSlice.actions;
+
+export const userReducer = userSlice.reducer;
+
+// above we have generated slice that not only replaces reducer but also type and action
+
+
+
+// export const userReducer = (state = INITIAL_STATE, action) => {
+//   const { type, payload } = action;
+
+//   switch (type) {
+//     case USER_ACTION_TYPES.SET_CURRENT_USER:
+//       return { ...state, currentUser: payload };
+//     default:
+//       return state;
+//   }
+// };
+
